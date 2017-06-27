927663f8b1452a0d96f01d9ddeb613db4772b1f4
Jason Wang <vuwij@me.com>
2017-06-27 14:21:09 -0400

    Added reference

--- matlab/experiments/calculate_dielectric_values.m
+++ matlab/experiments/calculate_dielectric_values.m
@@ -1,4 +1,9 @@
-%% Calculates the frequency/angularvelocity dependence graph 
+%% Calculates the frequency/angularvelocity dependence graph
+% Equation 7, 8 of http://images.remss.com/papers/rsspubs/Meissner_TGRS_2004_dielectric.pdf
+% Graph should look like figure 3
+% To test, enter the variables in the command window for the function and
+% press play
+
 function[y] = calculate_dielectric_values(material_index, ang_vel_min, ang_vel_max, precision)
     % Convert angular velocity to frequency
     
