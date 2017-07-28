291a47f0700a1bcb8f908c933df4b70bb85245d1
Jason Wang <vuwij@hotmail.com>
2017-07-28 14:31:06 -0400

    Redid chicken Experiment

--- matlab/experiments/6-20-2016 Modelling Signal Path/interpolation_filter.m
+++ matlab/experiments/6-20-2016 Modelling Signal Path/interpolation_filter.m
@@ -3,16 +3,16 @@
 function [S_rx, s_rx] = interpolation_filter(S_tx, S_tx_freq, R, R_freq)
     G = spline(R_freq, R, S_tx_freq);
     S_rx = G.*S_tx;
+    if(S_tx_freq(1) < 0)
+        S_rx(1:length(S_tx)/2) = 0;
+        S_rx = S_rx * 2;
+%         S_rx(1:length(S_tx)/2) = fliplr(S_rx(length(S_tx)/2+1:end));
+    end
+    
     title('Received Pulse Spectrum')
     xlabel('Frequency (GHz)')
     ylabel('Normalized Amplitude')
-
+    
     % Obtain the time domain from the Spectrum
-    s = S_rx ./ 2; 
-    s = [s fliplr(s)];
-    L = length(s);
-    s = s * L;
-
-    s_rx = ifft(s);
-    s_rx = s_rx(2:end-1);
+    s_rx = ifft(ifftshift(S_rx))*length(S_rx);
     clear s
