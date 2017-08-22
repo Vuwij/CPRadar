cf5eb86f6f087ce13d2171fbeedb93f93e53acbb
amitawoah <amitaxd@live.com>
2017-08-22 09:54:07 -0400

    Implemented downconversion without decimation

--- matlab/experiments/8-11-2017 Path Length Interference/obtain_baseline_manual_downconversion.m
+++ matlab/experiments/8-11-2017 Path Length Interference/obtain_baseline_manual_downconversion.m
@@ -1,22 +1,4 @@
-function [baseline] = obtain_baseline_manual_downconversion(frequency)
-    
-    if frequency==3
-        Fc=7.29e9;
-    end
-    if frequency==4
-        Fc=8.748e9;
-    end
-    
-    %Create a downconverter with a decimation factor of 2
-    dwnConv = dsp.DigitalDownConverter(...
-      'SampleRate',23.328e9,...
-      'DecimationFactor',2,...
-      'Bandwidth',1.5e9,...
-      'PassbandRipple',0.05,...
-      'StopbandAttenuation',60,...
-      'StopbandFrequencySource', 'Property',...
-      'StopbandFrequency',1.5e9,...
-      'CenterFrequency',Fc);
+function [baseline] = obtain_baseline_manual_downconversion(frequency) 
 
     table_name = strcat('rf','0_1400f',int2str(frequency));
     file_name = strcat('baseline2/baseline_zero.mat');
@@ -24,6 +6,6 @@ function [baseline] = obtain_baseline_manual_downconversion(frequency)
     baseline = S.(table_name);
     
     %Downconvert the rf data
-    baseline=dwnConv(baseline');
+    baseline=downconvert(baseline, frequency);
 end
 
